b67b32ca4abfc7c94ecdd521deb2e747ce83c4cd
Mehmet Çanacık <canacik.mehmet@gmail.com>
2021-11-20 20:08:43 +0300

    Users Updated

--- core/base/appUsers.js
+++ core/base/appUsers.js
@@ -1,46 +1,626 @@
 module.exports = [{
-        userId: 1,
-        name: {
-            fName: "Mehmet",
-            lName: "Çanacık"
+        "userId": 1,
+        "isActive": false,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Denise",
+            "lName": "Morales"
         },
-        age: 35,
-        favColor: ['blue', 'red']
+        "age": 24,
+        "company": "CIPROMOX",
+        "email": "denisemorales@cipromox.com",
+        "friends": [{
+                "id": 0,
+                "name": "Manning Bennett"
+            },
+            {
+                "id": 1,
+                "name": "Wilma Marshall"
+            },
+            {
+                "id": 2,
+                "name": "Ward Bradley"
+            }
+        ]
     },
     {
-        userId: 2,
-        name: {
-            fName: "İrem",
-            lName: "Çanacık"
+        "userId": 2,
+        "isActive": false,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Baker",
+            "lName": "Galloway"
         },
-        age: 34,
-        favColor: ['purple', 'red']
+        "age": 27,
+        "company": "PETICULAR",
+        "email": "bakergalloway@peticular.com",
+        "friends": [{
+                "id": 0,
+                "name": "Della Richard"
+            },
+            {
+                "id": 1,
+                "name": "Walton Mercado"
+            },
+            {
+                "id": 2,
+                "name": "Vasquez Norton"
+            }
+        ]
     },
     {
-        userId: 3,
-        name: {
-            fName: "Bilgehan",
-            lName: "Çanacık"
+        "userId": 3,
+        "isActive": false,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Rhoda",
+            "lName": "Hansen"
         },
-        age: 1,
-        favColor: null
+        "age": 21,
+        "company": "XERONK",
+        "email": "rhodahansen@xeronk.com",
+        "friends": [{
+                "id": 0,
+                "name": "Myrtle Ortega"
+            },
+            {
+                "id": 1,
+                "name": "Hale Barker"
+            },
+            {
+                "id": 2,
+                "name": "Juliana Johnston"
+            }
+        ]
     },
     {
-        userId: 4,
-        name: {
-            fName: "Asım",
-            lName: "Yılmaz"
+        "userId": 4,
+        "isActive": false,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Poole",
+            "lName": "Rogers"
         },
-        age: 45,
-        favColor: ['blue', 'black', 'yellow']
-    }, {
-        userId: 5,
-        name: {
-            fName: "İbrahim",
-            lName: "Güngör"
+        "age": 34,
+        "company": "IRACK",
+        "email": "poolerogers@irack.com",
+        "friends": [{
+                "id": 0,
+                "name": "Cherry Frank"
+            },
+            {
+                "id": 1,
+                "name": "Luz Lancaster"
+            },
+            {
+                "id": 2,
+                "name": "Meyer Peterson"
+            }
+        ]
+    },
+    {
+        "userId": 5,
+        "isActive": false,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Lucia",
+            "lName": "Vaughan"
         },
-        age: 64,
-        favColor: ['red']
+        "age": 38,
+        "company": "EXTRAGEN",
+        "email": "luciavaughan@extragen.com",
+        "friends": [{
+                "id": 0,
+                "name": "Bonner Bruce"
+            },
+            {
+                "id": 1,
+                "name": "Marquita Logan"
+            },
+            {
+                "id": 2,
+                "name": "Rhonda Owen"
+            }
+        ]
+    },
+    {
+        "userId": 6,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Alston",
+            "lName": "Mcbride"
+        },
+        "age": 28,
+        "company": "MARVANE",
+        "email": "alstonmcbride@marvane.com",
+        "friends": [{
+                "id": 0,
+                "name": "Lela Oneill"
+            },
+            {
+                "id": 1,
+                "name": "Allison Garner"
+            },
+            {
+                "id": 2,
+                "name": "Dale Sherman"
+            }
+        ]
+    },
+    {
+        "userId": 7,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Romero",
+            "lName": "Randolph"
+        },
+        "age": 25,
+        "company": "YOGASM",
+        "email": "romerorandolph@yogasm.com",
+        "friends": [{
+                "id": 0,
+                "name": "Janice Baldwin"
+            },
+            {
+                "id": 1,
+                "name": "Alexandra Holland"
+            },
+            {
+                "id": 2,
+                "name": "Tyson Key"
+            }
+        ]
+    },
+    {
+        "userId": 8,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Peck",
+            "lName": "Watkins"
+        },
+        "age": 37,
+        "company": "VORTEXACO",
+        "email": "peckwatkins@vortexaco.com",
+        "friends": [{
+                "id": 0,
+                "name": "Guthrie Sanchez"
+            },
+            {
+                "id": 1,
+                "name": "Strickland Chandler"
+            },
+            {
+                "id": 2,
+                "name": "Daugherty Molina"
+            }
+        ]
+    },
+    {
+        "userId": 9,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Morgan",
+            "lName": "Conrad"
+        },
+        "age": 40,
+        "company": "ZAJ",
+        "email": "morganconrad@zaj.com",
+        "friends": [{
+                "id": 0,
+                "name": "Kirsten Sutton"
+            },
+            {
+                "id": 1,
+                "name": "Gordon Hodges"
+            },
+            {
+                "id": 2,
+                "name": "Thornton Mendez"
+            }
+        ]
+    },
+    {
+        "userId": 10,
+        "isActive": false,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Robin",
+            "lName": "Wade"
+        },
+        "age": 35,
+        "company": "ZEAM",
+        "email": "robinwade@zeam.com",
+        "friends": [{
+                "id": 0,
+                "name": "Laura Orr"
+            },
+            {
+                "id": 1,
+                "name": "Pierce Hall"
+            },
+            {
+                "id": 2,
+                "name": "Selena Cox"
+            }
+        ]
+    },
+    {
+        "userId": 11,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Brandie",
+            "lName": "Knowles"
+        },
+        "age": 36,
+        "company": "KONGENE",
+        "email": "brandieknowles@kongene.com",
+        "friends": [{
+                "id": 0,
+                "name": "Navarro Austin"
+            },
+            {
+                "id": 1,
+                "name": "Everett Duffy"
+            },
+            {
+                "id": 2,
+                "name": "Ferguson Noble"
+            }
+        ]
+    },
+    {
+        "userId": 12,
+        "isActive": false,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Floyd",
+            "lName": "Nieves"
+        },
+        "age": 31,
+        "company": "ZERBINA",
+        "email": "floydnieves@zerbina.com",
+        "friends": [{
+                "id": 0,
+                "name": "Irwin Cotton"
+            },
+            {
+                "id": 1,
+                "name": "Waters Santana"
+            },
+            {
+                "id": 2,
+                "name": "Wells Ayala"
+            }
+        ]
+    },
+    {
+        "userId": 13,
+        "isActive": false,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Ophelia",
+            "lName": "Foley"
+        },
+        "age": 39,
+        "company": "ORBIFLEX",
+        "email": "opheliafoley@orbiflex.com",
+        "friends": [{
+                "id": 0,
+                "name": "Love Cummings"
+            },
+            {
+                "id": 1,
+                "name": "Lott Walsh"
+            },
+            {
+                "id": 2,
+                "name": "Rosario Vance"
+            }
+        ]
+    },
+    {
+        "userId": 14,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Carly",
+            "lName": "Boyd"
+        },
+        "age": 26,
+        "company": "KONNECT",
+        "email": "carlyboyd@konnect.com",
+        "friends": [{
+                "id": 0,
+                "name": "Lakeisha Downs"
+            },
+            {
+                "id": 1,
+                "name": "Noemi Pittman"
+            },
+            {
+                "id": 2,
+                "name": "Angela Battle"
+            }
+        ]
+    },
+    {
+        "userId": 15,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Cortez",
+            "lName": "Knight"
+        },
+        "age": 40,
+        "company": "NUTRALAB",
+        "email": "cortezknight@nutralab.com",
+        "friends": [{
+                "id": 0,
+                "name": "Dyer Pitts"
+            },
+            {
+                "id": 1,
+                "name": "Gabriela Hebert"
+            },
+            {
+                "id": 2,
+                "name": "Prince Townsend"
+            }
+        ]
+    },
+    {
+        "userId": 16,
+        "isActive": false,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Irma",
+            "lName": "Foster"
+        },
+        "age": 31,
+        "company": "HOMETOWN",
+        "email": "irmafoster@hometown.com",
+        "friends": [{
+                "id": 0,
+                "name": "Mary Chapman"
+            },
+            {
+                "id": 1,
+                "name": "Walker Paul"
+            },
+            {
+                "id": 2,
+                "name": "Roseann Carver"
+            }
+        ]
+    },
+    {
+        "userId": 17,
+        "isActive": false,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Tina",
+            "lName": "Cole"
+        },
+        "age": 35,
+        "company": "PHOTOBIN",
+        "email": "tinacole@photobin.com",
+        "friends": [{
+                "id": 0,
+                "name": "Moody Melton"
+            },
+            {
+                "id": 1,
+                "name": "Kathleen Campbell"
+            },
+            {
+                "id": 2,
+                "name": "Garner Barrett"
+            }
+        ]
+    },
+    {
+        "userId": 18,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Casey",
+            "lName": "Williams"
+        },
+        "age": 26,
+        "company": "UNQ",
+        "email": "caseywilliams@unq.com",
+        "friends": [{
+                "id": 0,
+                "name": "Gabrielle Gentry"
+            },
+            {
+                "id": 1,
+                "name": "Forbes Mayer"
+            },
+            {
+                "id": 2,
+                "name": "Blevins William"
+            }
+        ]
+    },
+    {
+        "userId": 19,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Madge",
+            "lName": "Savage"
+        },
+        "age": 40,
+        "company": "EVEREST",
+        "email": "madgesavage@everest.com",
+        "friends": [{
+                "id": 0,
+                "name": "Hoover Russell"
+            },
+            {
+                "id": 1,
+                "name": "Chan Nichols"
+            },
+            {
+                "id": 2,
+                "name": "Reynolds Tucker"
+            }
+        ]
+    },
+    {
+        "userId": 20,
+        "isActive": false,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Consuelo",
+            "lName": "Mcdonald"
+        },
+        "age": 28,
+        "company": "PORTICA",
+        "email": "consuelomcdonald@portica.com",
+        "friends": [{
+                "id": 0,
+                "name": "Miller Merrill"
+            },
+            {
+                "id": 1,
+                "name": "Hope Mejia"
+            },
+            {
+                "id": 2,
+                "name": "Deena Padilla"
+            }
+        ]
+    },
+    {
+        "userId": 21,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Sparks",
+            "lName": "Howard"
+        },
+        "age": 39,
+        "company": "SCENTY",
+        "email": "sparkshoward@scenty.com",
+        "friends": [{
+                "id": 0,
+                "name": "Barr Hancock"
+            },
+            {
+                "id": 1,
+                "name": "Geneva Jenkins"
+            },
+            {
+                "id": 2,
+                "name": "Buckley Schroeder"
+            }
+        ]
+    },
+    {
+        "userId": 22,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Anthony",
+            "lName": "Cohen"
+        },
+        "age": 20,
+        "company": "ZIPAK",
+        "email": "anthonycohen@zipak.com",
+        "friends": [{
+                "id": 0,
+                "name": "Campbell Silva"
+            },
+            {
+                "id": 1,
+                "name": "Debbie Dorsey"
+            },
+            {
+                "id": 2,
+                "name": "Bradshaw Whitehead"
+            }
+        ]
+    },
+    {
+        "userId": 23,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Rosales",
+            "lName": "Miles"
+        },
+        "age": 35,
+        "company": "KOFFEE",
+        "email": "rosalesmiles@koffee.com",
+        "friends": [{
+                "id": 0,
+                "name": "Spencer Hendrix"
+            },
+            {
+                "id": 1,
+                "name": "Deirdre Blevins"
+            },
+            {
+                "id": 2,
+                "name": "Annie Crane"
+            }
+        ]
+    },
+    {
+        "userId": 24,
+        "isActive": true,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Stewart",
+            "lName": "Duncan"
+        },
+        "age": 34,
+        "company": "STREZZO",
+        "email": "stewartduncan@strezzo.com",
+        "friends": [{
+                "id": 0,
+                "name": "Edna Rice"
+            },
+            {
+                "id": 1,
+                "name": "Cheryl Page"
+            },
+            {
+                "id": 2,
+                "name": "Sylvia Roberts"
+            }
+        ]
+    },
+    {
+        "userId": 25,
+        "isActive": false,
+        "picture": "http://placehold.it/32x32",
+        "name": {
+            "fName": "Trujillo",
+            "lName": "Conley"
+        },
+        "age": 37,
+        "company": "UNDERTAP",
+        "email": "trujilloconley@undertap.com",
+        "friends": [{
+                "id": 0,
+                "name": "Brennan Haynes"
+            },
+            {
+                "id": 1,
+                "name": "Strong Bonner"
+            },
+            {
+                "id": 2,
+                "name": "Emily Saunders"
+            }
+        ]
     }
-
 ];
