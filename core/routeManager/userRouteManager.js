b84c014bfc3cf821e9ef594620d0553fcf1eaf78
Mehmet Çanacık <canacik.mehmet@gmail.com>
2021-11-20 19:48:37 +0300

    Get User and Users Added

--- core/routeManager/userRouteManager.js
+++ core/routeManager/userRouteManager.js
@@ -4,4 +4,17 @@ baseRouter.get('/', (getReq, getRes) => {
     getRes.send(users);
 });
 
+baseRouter.get('/:userId', (getReq, getRes) => {
+
+    let tempResult = (users.find((user) => user.userId == getReq.params.userId));
+    console.log(tempResult);
+    if (tempResult !=undefined) {
+        return getRes.json(tempResult);
+    }
+    return getRes.json({
+        userId: 0,
+        description: "User Not Found!"
+    })
+});
+
 module.exports = baseRouter;
